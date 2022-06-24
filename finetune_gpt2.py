d9ab44bcb82b894400622afea2718fcf93eb71ae
Miguel Frutos <89743376+miguelfrutos@users.noreply.github.com>
2022-06-24 09:12:42 +0200

    Update finetune_gpt2.py

--- finetune_gpt2.py
+++ finetune_gpt2.py
@@ -1,5 +1,6 @@
 #pip install gpt_2_simple
 import gpt_2_simple as gpt2
+import configparser
 
 # Download the small version of GPT2
 gpt2.download_gpt2(model_name='124M')
@@ -12,22 +13,22 @@ sess = gpt2.start_tf_sess()
 gpt2.finetune(sess,
               dataset=file_name,
               model_name='124M',
-              steps=200,
+              steps=20,
               restore_from='fresh',
               run_name='run1',
               print_every=10,
               sample_every=20,
               )
 
-# run_test
-gpt2.generate(sess,run_name='run1')
+# Envionment Variables with ConfigParser to include the starting point
+cfg = configparser.ConfigParser()
+cfg.read('user.ini')
+input_lyrics=cfg.get('INPUTS','input_lyrics')
 
 # run_test with some parameters
-input_lyrics=cfg.get('INPUTS','input_lyrics')
 gpt2.generate(sess,
               length=250,
               temperature=0.7,
               prefix=input_lyrics,
               nsamples=5,
               top_k=40)
-
